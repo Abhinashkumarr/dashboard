4b81c22776d313c64953b1e21e1be37773352ac5
Abhinash Kumar <abhinash@mycareershapers.in>
2023-11-04 17:40:14 +0530

    finalchange

--- app/api/auth/[kindeAuth]/route.ts
+++ app/api/auth/[kindeAuth]/route.ts
@@ -1,33 +1,8 @@
 import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
-// import { handleAuth } from "@kinde-oss/kinde-auth-nextjs";
 
-// export async function GET(request: object | undefined, {params}: any) {
-//     const endpoint = params.kindeAuth;
-//     return await handleAuth(request, endpoint);
-// }
-
-// Import the necessary function from the package
-
-// Your serverless function to handle a GET request
-export async function GET(request: object | undefined, { params }: any) {
+export async function GET(request: object, {params}: any) {
     const endpoint = params.kindeAuth;
-
-    try {
-        // Assuming handleAuth returns a promise or can be awaited
-        const authResult = await handleAuth(request, endpoint);
-
-        // Perform any necessary operations with authResult if required
-        // ...
-
-        return {
-            status: 200,
-            body: JSON.stringify(authResult) // Modify the response as per your use case
-        };
-    } catch (error) {
-        // Handle errors gracefully
-        return {
-            status: 500,
-            body: JSON.stringify({ error: "An error occurred during authentication" })
-        };
-    }
+    return await handleAuth(request, endpoint);
 }
+
+
